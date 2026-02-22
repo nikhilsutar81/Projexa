c06ef71a8ad4ff00729da1ac04c758d69e41eb7c
nikhilsutar81 <nikhilsutar81@gmail.com>
2026-02-22 20:55:45 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
+import { protect } from "./middlewares/authMiddleware.js";
+import projectRouter from "./routes/projectRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => res.send("Server is live!⌛"));
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(
+    `Server is running on port ${PORT} => http://localhost:${PORT} 🚀`
+  )
+);
